036dced0b6e194d174a06332422da583ea36102c
KJ OBrien <kjokjo219@gmail.com>
2025-10-15 21:07:09 -0400

    adds todays game end point

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-
+from nba_api.live.nba.endpoints import scoreboard, boxscore, odds, playbyplay
 # Create FastAPI instance
 app = FastAPI()
+
+todays_games = []
 
 origins = [
     "http://localhost:4203",
@@ -17,10 +19,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"hello": "world"}
+@app.get("/todays-games")
+def get_todays_game_ids():
+    todays_games = set_todays_games()
+    return todays_games
+    
+def set_todays_games():
+    response = scoreboard.ScoreBoard().get_dict()["scoreboard"]["games"]
+    games = []
+    print(response)
+
+    for i in range(len(response)):
+        games.append(
+            {
+                "gameId": response[i]["gameId"],
+                "homeTeam": response[i]["homeTeam"]["teamName"],
+                "homeTeamWins":  response[i]["homeTeam"]["wins"],
+                "homeTeamLoses": response[i]["homeTeam"]["losses"],
+                "awayTeam": response[i]["awayTeam"]["teamName"],
+                "awayTeamWins":  response[i]["awayTeam"]["wins"],
+                "awayTeamLoses": response[i]["awayTeam"]["losses"],
+            }
+            )
+
+    return games
 
 
 if __name__ == "__main__":
+    
     uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=True)
